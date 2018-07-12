8df87921a12fcf715740fcb549a807e473e5e33a
dmartin <david@dadahasa.com>
2018-07-12 13:01:24 -0500

    Add fragment message to hoast activity --Send the toast message from host activity instead of fragment

--- app/src/main/java/com/dadahasa/baking_app/ui/RecipeActivity.java
+++ app/src/main/java/com/dadahasa/baking_app/ui/RecipeActivity.java
@@ -2,13 +2,20 @@ package com.dadahasa.baking_app.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.dadahasa.baking_app.R;
 
-public class RecipeActivity extends AppCompatActivity {
+public class RecipeActivity extends AppCompatActivity implements RecipeListFragment.OnRecipeClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+    }
+
+    @Override
+    public void onRecipeSelected(int position) {
+        Toast.makeText(this, "Recipe Clicked " + position, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/dadahasa/baking_app/ui/RecipeListFragment.java
+++ app/src/main/java/com/dadahasa/baking_app/ui/RecipeListFragment.java
@@ -1,6 +1,7 @@
 package com.dadahasa.baking_app.ui;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,7 +15,6 @@ import com.dadahasa.baking_app.R;
 import com.dadahasa.baking_app.RecipeApi;
 import com.dadahasa.baking_app.model.Recipe;
 
-import java.util.Arrays;
 import java.util.List;
 
 import retrofit2.Call;
@@ -37,6 +37,27 @@ public class RecipeListFragment extends Fragment {
     public RecipeListFragment(){
     }
 
+    //callback to pass the recipe index clicked to the host activity
+    OnRecipeClickListener mCallback;
+
+    //this interface calls a method in the host activity, onRecipeSelected,
+    //to pass the recipe index clicked
+    public interface OnRecipeClickListener {
+        void onRecipeSelected(int position);
+    }
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        //this makes sure the host activity has implemented the callback interface
+        try {
+            mCallback = (OnRecipeClickListener) context;
+        } catch (ClassCastException e){
+            throw new ClassCastException(context.toString() + "must implement onRecipeClickListener");
+        }
+    }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
@@ -58,7 +79,7 @@ public class RecipeListFragment extends Fragment {
 
         //return the FRAGMENT view to be placed in the list view of the fragment element
         return rootView;
-        //return listView;
+
     }
 
 
@@ -71,7 +92,9 @@ public class RecipeListFragment extends Fragment {
 
     private void onRecipeClicked(AdapterView<?> parent, View view, int position, long id){
         //put here what we want it to happen when a list item is clicked
-        Toast.makeText(getContext(), "Recipe Clicked " + position, Toast.LENGTH_LONG).show();
+        //Toast.makeText(getContext(), "Recipe Clicked " + position, Toast.LENGTH_LONG).show();
+        //This calls the method onRecipeSelected on the host activity
+        mCallback.onRecipeSelected(position);
     }
 
 
